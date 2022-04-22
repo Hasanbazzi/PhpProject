6d3bb3cb7136ef0d20dddbc64ac5fce5d97178bb
BaZzi1 <65305485+BaZzi1@users.noreply.github.com>
2022-04-22 20:01:27 +0300

    Add files via upload

---
+++ food-project/admin/partials/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+        <div class="wrapper">
+            <p class="text-center">All rights reserved. Developed By <a href="#">Hassan Bazzi</a></p>
+        </div>
+    </div>
+	</body>
+	</html>
